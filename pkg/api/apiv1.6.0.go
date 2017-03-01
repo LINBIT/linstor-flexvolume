8ffa23827cb88d8a606252023b6ff3a7c31b3342
Hayley Swimelar <hayley@linbit.com>
2017-03-01 13:26:48 -0800

    dummy api calls

--- pkg/api/apiv1.6.0.go
+++ pkg/api/apiv1.6.0.go
@@ -1,5 +1,9 @@
 package api
 
+import (
+	"encoding/json"
+)
+
 func init() {
 	flexAPIs = append(flexAPIs, v160{})
 }
@@ -12,6 +16,16 @@ type responce struct {
 type attachResponce struct {
 	responce
 	Device string `json:"device"`
+}
+
+type isAttachedResponce struct {
+	responce
+	Attached string `json:"attached"`
+}
+
+type getVolNameResponce struct {
+	responce
+	VolumeName string `json:"volumeName"`
 }
 
 type options struct {
@@ -31,4 +45,57 @@ func (v v160) apiVersion() string {
 
 func (v v160) Call(s string) string {
 	return "{\"status\": \"Failure\"}"
+}
+
+func (v v160) init() string {
+	res, _ := json.Marshal(responce{Status: "Success"})
+	return string(res)
+}
+
+func (v v160) attach(s []string) string {
+	res, _ := json.Marshal(attachResponce{
+		Device: "/dev/drbd100",
+		responce: responce{
+			Status: "Success",
+		},
+	})
+	return string(res)
+}
+
+func (v v160) waitForAttach(s []string) string {
+	res, _ := json.Marshal(responce{Status: "Success"})
+	return string(res)
+}
+
+func (v v160) detach(s []string) string {
+	res, _ := json.Marshal(responce{Status: "Success"})
+	return string(res)
+}
+
+func (v v160) mountDevice(s []string) string {
+	res, _ := json.Marshal(responce{Status: "Success"})
+	return string(res)
+}
+
+func (v v160) unmountDevice(s []string) string {
+	res, _ := json.Marshal(responce{Status: "Success"})
+	return string(res)
+}
+
+func (v v160) getVolumeName(s []string) string {
+	res, _ := json.Marshal(getVolNameResponce{
+		VolumeName: "test0",
+		responce: responce{
+			Status: "Success",
+		},
+	})
+	return string(res)
+}
+
+func (v v160) isAttached(s []string) string {
+	res, _ := json.Marshal(isAttachedResponce{
+		Attached: "true",
+		responce: responce{Status: "Success"},
+	})
+	return string(res)
 }
