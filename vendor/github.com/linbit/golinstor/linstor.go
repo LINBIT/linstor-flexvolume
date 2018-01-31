5ef7d20c8bf25ebf1e5c58781d460fbc79f948be
Hayley Swimelar <hayley@linbit.com>
2018-01-31 15:02:51 -0800

    vendor in golinstor

---
+++ vendor/github.com/linbit/golinstor/linstor.go
@@ -0,0 +1,519 @@
+/*
+* A helpful library to interact with Linstor
+* Copyright © 2018 LINBIT USA LCC
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package linstor
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+// Resource contains all the information needed to query and assign/deploy
+// a resource. If you're deploying a resource, Redundancy is required. If you're
+// assigning a resource to a particular node, NodeName is required.
+type Resource struct {
+	Name        string
+	NodeName    string
+	Redundancy  string
+	NodeList    []string
+	ClientList  []string
+	StoragePool string
+	SizeKiB     uint64
+}
+
+type resList []struct {
+	ResourceStates []struct {
+		RequiresAdjust bool      `json:"requires_adjust"`
+		RscName        string    `json:"rsc_name"`
+		IsPrimary      bool      `json:"is_primary"`
+		VlmStates      []volInfo `json:"vlm_states"`
+		IsPresent      bool      `json:"is_present"`
+		NodeName       string    `json:"node_name"`
+	} `json:"resource_states"`
+	Resources []resInfo `json:"resources"`
+}
+type resInfo struct {
+	Vlms []struct {
+		VlmNr        int    `json:"vlm_nr"`
+		StorPoolName string `json:"stor_pool_name"`
+		StorPoolUUID string `json:"stor_pool_uuid"`
+		VlmMinorNr   int    `json:"vlm_minor_nr"`
+		VlmUUID      string `json:"vlm_uuid"`
+		VlmDfnUUID   string `json:"vlm_dfn_uuid"`
+	} `json:"vlms"`
+	NodeUUID string `json:"node_uuid"`
+	UUID     string `json:"uuid"`
+	NodeName string `json:"node_name"`
+	Props    []struct {
+		Value string `json:"value"`
+		Key   string `json:"key"`
+	} `json:"props"`
+	RscDfnUUID string   `json:"rsc_dfn_uuid"`
+	Name       string   `json:"name"`
+	RscFlags   []string `json:"rsc_flags,omitempty"`
+}
+
+type volInfo struct {
+	HasDisk       bool `json:"has_disk"`
+	CheckMetaData bool `json:"check_meta_data"`
+	HasMetaData   bool `json:"has_meta_data"`
+	IsPresent     bool `json:"is_present"`
+	DiskFailed    bool `json:"disk_failed"`
+	NetSize       int  `json:"net_size"`
+	VlmMinorNr    int  `json:"vlm_minor_nr"`
+	GrossSize     int  `json:"gross_size"`
+	VlmNr         int  `json:"vlm_nr"`
+}
+
+type returnStatuses []struct {
+	DetailsFormat string `json:"details_format"`
+	MessageFormat string `json:"message_format"`
+	CauseFormat   string `json:"cause_format,omitempty"`
+	ObjRefs       []struct {
+		Value string `json:"value"`
+		Key   string `json:"key"`
+	} `json:"obj_refs"`
+	Variables []struct {
+		Value string `json:"value"`
+		Key   string `json:"key"`
+	} `json:"variables"`
+	RetCode uint64 `json:"ret_code"`
+}
+
+type resDefInfo []struct {
+	RscDfns []struct {
+		VlmDfns []struct {
+			VlmDfnUUID string `json:"vlm_dfn_uuid"`
+			VlmMinor   int    `json:"vlm_minor"`
+			VlmNr      int    `json:"vlm_nr"`
+			VlmSize    int    `json:"vlm_size"`
+		} `json:"vlm_dfns,omitempty"`
+		RscDfnSecret string `json:"rsc_dfn_secret"`
+		RscDfnUUID   string `json:"rsc_dfn_uuid"`
+		RscName      string `json:"rsc_name"`
+		RscDfnPort   int    `json:"rsc_dfn_port"`
+		RscDfnProps  []struct {
+			Value string `json:"value"`
+			Key   string `json:"key"`
+		} `json:"rsc_dfn_props,omitempty"`
+	} `json:"rsc_dfns"`
+}
+
+func (s returnStatuses) validate() error {
+	for _, message := range s {
+		if !linstorSuccess(message.RetCode) {
+			msg, err := json.Marshal(s)
+			if err != nil {
+				return err
+			}
+			return fmt.Errorf("error status from one or more linstor operations: %s", msg)
+		}
+	}
+	return nil
+}
+
+func linstorSuccess(retcode uint64) bool {
+	const (
+		maskError = 0xC000000000000000
+		maskWarn  = 0x8000000000000000
+		maskInfo  = 0x4000000000000000
+	)
+	return (retcode & (maskError | maskWarn | maskInfo)) == 0
+}
+
+// CreateAndAssign deploys the resource, created a new one if it doesn't exist.
+func (r Resource) CreateAndAssign() error {
+	if err := r.Create(); err != nil {
+		return err
+	}
+	if err := r.Assign(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Only use this for things that return the normal returnStatuses json.
+func linstor(args ...string) error {
+	args = append([]string{"-m"}, args...)
+	out, err := exec.Command("linstor", args...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%v : %s", err, out)
+	}
+
+	s := returnStatuses{}
+	if err := json.Unmarshal(out, &s); err != nil {
+		return fmt.Errorf("couldn't Unmarshal %s :%v", out, err)
+	}
+
+	if err := s.validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Create reserves the resource name in Linstor.
+func (r Resource) Create() error {
+	out, err := exec.Command("linstor", "-m", "list-resource-definition").CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%v: %s", err, out)
+	}
+	s := resDefInfo{}
+	if err := json.Unmarshal(out, &s); err != nil {
+		return fmt.Errorf("couldn't Unmarshal %s :%v", out, err)
+	}
+
+	var defPresent bool
+	var volZeroPresent bool
+
+	for _, def := range s[0].RscDfns {
+		if def.RscName == r.Name {
+			defPresent = true
+			for _, vol := range def.VlmDfns {
+				if vol.VlmNr == 0 {
+					volZeroPresent = true
+				}
+			}
+		}
+	}
+
+	if !defPresent {
+		if err := linstor("create-resource-definition", r.Name); err != nil {
+			return fmt.Errorf("unable to reserve resource name %s :%v", r.Name, err)
+		}
+	}
+
+	if !volZeroPresent {
+		if err := linstor("create-volume-definition", r.Name, fmt.Sprintf("%dkib", r.SizeKiB)); err != nil {
+			return fmt.Errorf("unable to reserve resource name %s :%v", r.Name, err)
+		}
+	}
+
+	return nil
+}
+
+// Assign assigns a resource with diskfull storage to all nodes in its NodeList,
+// then attaches the resource disklessly to all nodes in its ClientList.
+func (r Resource) Assign() error {
+	// Make sure the resource is defined before trying to assign it.
+	//	ok, err := r.Exists()
+	//	if err != nil {
+	//		return fmt.Errorf("Unable to determine if resource %s is defined %v", r.Name, err)
+	//	}
+	//	if !ok {
+	//		return fmt.Errorf("No resource definition for resource %s", r.Name)
+	//	}
+	//
+	for _, node := range r.NodeList {
+		present, err := r.OnNode(node)
+		if err != nil {
+			return fmt.Errorf("unable to assign resource %s failed to check if it was already present on node %s: %v", r.Name, node, err)
+		}
+		if !present {
+			if err = linstor("create-resource", r.Name, node, "-s", r.StoragePool); err != nil {
+				return err
+			}
+		}
+	}
+
+	for _, node := range r.ClientList {
+		present, err := r.OnNode(node)
+		if err != nil {
+			return fmt.Errorf("unable to assign resource %s failed to check if it was already present on node %s: %v", r.Name, node, err)
+		}
+		if !present {
+			if err = linstor("create-resource", r.Name, node, "--diskless"); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Unassign unassigns a resource from a particular node.
+func (r Resource) Unassign(nodeName string) error {
+	if err := linstor("delete-resource", r.Name, nodeName); err != nil {
+		return fmt.Errorf("failed to unassign resource %s from node %s: %v", r.Name, nodeName, err)
+	}
+	return nil
+}
+
+// Delete removes a resource entirely from all nodes.
+func (r Resource) Delete() error {
+	if err := linstor("delete-resource-definition", r.Name); err != nil {
+		return fmt.Errorf("failed to delete resource %s: %v", r.Name, err)
+	}
+	return nil
+}
+
+// Exists checks to see if a resource is defined in DRBD Manage.
+func (r Resource) Exists() (bool, error) {
+	out, err := exec.Command("linstor", "-m", "ls-rsc").CombinedOutput()
+	if err != nil {
+		return false, err
+	}
+
+	// Inject real implementations here, test through the internal function.
+	return doResExists(r.Name, out)
+}
+
+func doResExists(resourceName string, resInfo []byte) (bool, error) {
+	resources := resList{}
+
+	err := json.Unmarshal(resInfo, &resources)
+	if err != nil {
+		return false, fmt.Errorf("couldn't Unmarshal %s :%v", resInfo, err)
+	}
+
+	for _, r := range resources[0].Resources {
+		if r.Name == resourceName {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+//OnNode determines if a resource is present on a particular node.
+func (r Resource) OnNode(nodeName string) (bool, error) {
+	out, err := exec.Command("linstor", "-m", "ls-rsc").CombinedOutput()
+	if err != nil {
+		return false, err
+	}
+
+	l := resList{}
+	if err := json.Unmarshal(out, &l); err != nil {
+		return false, fmt.Errorf("couldn't Unmarshal %s :%v", out, err)
+	}
+
+	return doResOnNode(l, r.Name, nodeName), nil
+}
+
+func doResOnNode(list resList, resName, nodeName string) bool {
+	for _, res := range list[0].Resources {
+		if res.Name == resName && res.NodeName == nodeName {
+			return true
+		}
+	}
+	return false
+}
+
+// IsClient determines if resource is running as a client on nodeName.
+func (r Resource) IsClient(nodeName string) bool {
+	out, _ := exec.Command("linstor", "-m", "ls-rsc").CombinedOutput()
+
+	list := resList{}
+	if err := json.Unmarshal(out, &list); err != nil {
+		return false
+	}
+
+	return r.doIsClient(list, nodeName)
+}
+
+func (r Resource) doIsClient(list resList, nodeName string) bool {
+	// Traverse all the volume states to find volume 0 of our resource on nodeName.
+	// Assume volume 0 is the one we want.
+	for _, res := range list[0].ResourceStates {
+		if r.Name == res.RscName && nodeName == res.NodeName {
+			for _, v := range res.VlmStates {
+				if v.VlmNr == 0 {
+					return !v.HasDisk
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+// EnoughFreeSpace checks to see if there's enough free space to create a new resource.
+func EnoughFreeSpace(requestedKiB, replicas string) error {
+	return nil
+}
+
+// FSUtil handles creating a filesystem and mounting resources.
+type FSUtil struct {
+	*Resource
+	FSType string
+}
+
+// Mount the FSUtil's resource on the path.
+func (f FSUtil) Mount(path string) error {
+	device, err := WaitForDevPath(*f.Resource, 3)
+	if err != nil {
+		return fmt.Errorf("unable to mount device, couldn't find Resource device path: %v", err)
+	}
+
+	err = f.safeFormat(device)
+	if err != nil {
+		return fmt.Errorf("unable to mount device: %v", err)
+	}
+
+	out, err := exec.Command("mkdir", "-p", path).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("unable to mount device, failed to make mount directory: %v: %s", err, out)
+	}
+
+	out, err = exec.Command("mount", device, path).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("unable to mount device: %v: %s", err, out)
+	}
+
+	return nil
+}
+
+// UnMount the FSUtil's resource from the path.
+func (f FSUtil) UnMount(path string) error {
+	// If the path isn't a directory, we're not mounted there.
+	_, err := exec.Command("test", "-d", path).CombinedOutput()
+	if err != nil {
+		return nil
+	}
+
+	// If the path isn't mounted, then we're not mounted.
+	_, err = exec.Command("findmnt", "-f", path).CombinedOutput()
+	if err != nil {
+		return nil
+	}
+
+	out, err := exec.Command("umount", path).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("unable to unmount device: %q: %s", err, out)
+	}
+
+	return nil
+}
+
+func (f FSUtil) safeFormat(path string) error {
+	deviceFS, err := checkFSType(path)
+	if err != nil {
+		return fmt.Errorf("unable to format filesystem for %q: %v", path, err)
+	}
+
+	// Device is formatted correctly already.
+	if deviceFS == f.FSType {
+		return nil
+	}
+
+	if deviceFS != "" && deviceFS != f.FSType {
+		return fmt.Errorf("device %q already formatted with %q filesystem, refusing to overwrite with %q filesystem", path, deviceFS, f.FSType)
+	}
+
+	out, err := exec.Command("mkfs", "-t", f.FSType, path).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("couldn't create %s filesystem %v: %q", f.FSType, err, out)
+	}
+
+	return nil
+}
+
+func checkFSType(dev string) (string, error) {
+	// If there's no filesystem, then we'll have a nonzero exit code, but no output
+	// doCheckFSType handles this case.
+	out, _ := exec.Command("blkid", "-o", "udev", dev).CombinedOutput()
+
+	FSType, err := doCheckFSType(string(out))
+	if err != nil {
+		return "", err
+	}
+	return FSType, nil
+}
+
+// Parse the filesystem from the output of `blkid -o udev`
+func doCheckFSType(s string) (string, error) {
+	f := strings.Fields(s)
+
+	// blkid returns an empty string if there's no filesystem and so do we.
+	if len(f) == 0 {
+		return "", nil
+	}
+
+	blockAttrs := make(map[string]string)
+	for _, pair := range f {
+		p := strings.Split(pair, "=")
+		if len(p) < 2 {
+			return "", fmt.Errorf("couldn't parse filesystem data from %s", s)
+		}
+		blockAttrs[p[0]] = p[1]
+	}
+
+	FSKey := "ID_FS_TYPE"
+	fs, ok := blockAttrs[FSKey]
+	if !ok {
+		return "", fmt.Errorf("couldn't find %s in %s", FSKey, blockAttrs)
+	}
+	return fs, nil
+}
+
+// WaitForDevPath polls until the resourse path appears on the system.
+func WaitForDevPath(r Resource, maxRetries int) (string, error) {
+	var path string
+	var err error
+
+	for i := 0; i < maxRetries; i++ {
+		path, err = getDevPath(r)
+		if path != "" {
+			return path, err
+		}
+		time.Sleep(time.Second * 2)
+	}
+	return path, err
+}
+
+func getDevPath(r Resource) (string, error) {
+	out, err := exec.Command("linstor", "-m", "ls-rsc").CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	list := resList{}
+	if err := json.Unmarshal(out, &list); err != nil {
+		return "", err
+	}
+
+	// Traverse all the volume states to find volume 0 of our resource.
+	// Assume volume 0 is the one we want.
+	vol := &volInfo{}
+	for _, res := range list[0].ResourceStates {
+		if r.Name == res.RscName {
+			for _, v := range res.VlmStates {
+				if v.VlmNr == 0 {
+					vol = &v
+				}
+			}
+		}
+	}
+
+	devicePath := doGetDevPath(*vol)
+
+	if _, err := os.Lstat(devicePath); err != nil {
+		return "", fmt.Errorf("Couldn't stat %s: %v", devicePath, err)
+	}
+
+	return devicePath, nil
+}
+
+func doGetDevPath(vol volInfo) string {
+	return fmt.Sprintf("/dev/drbd%d", +vol.VlmMinorNr)
+}
---
+++ vendor/github.com/linbit/golinstor/linstor_test.go
@@ -0,0 +1,59 @@
+/*
+* A helpful library to interact with Linstor
+* Copyright © 2018 LINBIT USA LCC
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package linstor
+
+import "testing"
+
+func TestDoResExists(t *testing.T) {
+
+	testOut1 := []byte("[\n  {\n    \"resources\": [\n      {\n        \"vlms\": [\n          {\n            \"vlm_dfn_uuid\": \"8000558f-9061-4256-b0fe-ec8d3b7fc051\", \n            \"stor_pool_uuid\": \"2d840084-f071-4173-a509-9a56d695a606\", \n            \"vlm_uuid\": \"ff382153-d47c-4a9f-a466-4ef4b3268656\", \n            \"vlm_nr\": 0, \n            \"stor_pool_name\": \"drbdpool\"\n          }, \n          {\n            \"vlm_dfn_uuid\": \"f763311c-3eb2-4f0e-ad55-361d4d026346\", \n            \"stor_pool_uuid\": \"2d840084-f071-4173-a509-9a56d695a606\", \n            \"vlm_uuid\": \"02a24f94-03a8-4918-8173-506b4cca369c\", \n            \"vlm_nr\": 1, \n            \"stor_pool_name\": \"drbdpool\"\n          }\n        ], \n        \"node_uuid\": \"1efa3fdf-129a-4018-a3f2-3d0f46571c5b\", \n        \"uuid\": \"d34e324a-54d8-4c79-adf5-3da33eca3371\", \n        \"node_name\": \"kubelet-a\", \n        \"props\": [\n          {\n            \"value\": \"drbdpool\", \n            \"key\": \"StorPoolName\"\n          }\n        ], \n        \"rsc_dfn_uuid\": \"83c4ce34-1bb5-48a7-a153-457f056223e1\", \n        \"name\": \"r00\"\n      }, \n      {\n        \"rsc_flags\": [\n          \"DELETE\"\n        ], \n        \"vlms\": [\n          {\n            \"vlm_nr\": 0, \n            \"stor_pool_name\": \"drbdpool\", \n            \"stor_pool_uuid\": \"ea3ddc0b-bc4f-4860-ae38-744c65cb3d18\", \n            \"vlm_uuid\": \"aebe934e-cda3-4b67-8d08-48c9444d12eb\", \n            \"vlm_dfn_uuid\": \"8000558f-9061-4256-b0fe-ec8d3b7fc051\", \n            \"vlm_flags\": [\n              \"DELETE\"\n            ]\n          }, \n          {\n            \"vlm_nr\": 1, \n            \"stor_pool_name\": \"drbdpool\", \n            \"stor_pool_uuid\": \"ea3ddc0b-bc4f-4860-ae38-744c65cb3d18\", \n            \"vlm_uuid\": \"36e679a3-d59a-4147-8f1e-299ae689c1d1\", \n            \"vlm_dfn_uuid\": \"f763311c-3eb2-4f0e-ad55-361d4d026346\", \n            \"vlm_flags\": [\n              \"DELETE\"\n            ]\n          }\n        ], \n        \"node_uuid\": \"c361253c-4e75-48d5-b00a-8a3df5eb3a69\", \n        \"uuid\": \"12a54b0c-5d76-4766-b973-26cfb1beaae4\", \n        \"node_name\": \"kubelet-b\", \n        \"props\": [\n          {\n            \"value\": \"drbdpool\", \n            \"key\": \"StorPoolName\"\n          }\n        ], \n        \"rsc_dfn_uuid\": \"83c4ce34-1bb5-48a7-a153-457f056223e1\", \n        \"name\": \"r00\"\n      }\n    ]\n  }\n]")
+	var resInfoTests = []struct {
+		resource string
+		resInfo  []byte
+		out      bool
+	}{
+		{"r00", testOut1, true},
+		{"wrong-o", testOut1, false},
+	}
+
+	for _, tt := range resInfoTests {
+		ok, _ := doResExists(tt.resource, tt.resInfo)
+		if ok != tt.out {
+			t.Errorf("Called: doResExists(%q, %q), Expected: %v, Got: %v", tt.resource, tt.resInfo, tt.out, ok)
+		}
+	}
+}
+
+func TestDoCheckFSType(t *testing.T) {
+	var blkidOutputStringTests = []struct {
+		in  string
+		out string
+	}{
+		{"ID_FS_UUID=15336bdb-4584-4c30-9719-754f5c4744e1\nID_FS_UUID_ENC=15336bdb-4584-4c30-9719-754f5c4744e1\nID_FS_TYPE=ext4\n", "ext4"},
+		{"ID_FS_UUID=15336bdb-4584-4c30-9719-754f5c4744e1\nID_FS_UUID_ENC=15336bdb-4584-4c30-9719-754f5c4744e1\nID_FS_TYPE=xfs\n", "xfs"},
+		{"\n", ""},
+	}
+
+	for _, tt := range blkidOutputStringTests {
+		FSType, _ := doCheckFSType(tt.in)
+		if FSType != tt.out {
+			t.Errorf("Called: doCheckFSType(%q), Expected: %q, Got: %q", tt.in, tt.out, FSType)
+		}
+	}
+}
